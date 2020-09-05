7f5928f262bac2b3250a56f695e6964795d50f48
ferran1 <ferrantl@live.nl>
2020-09-05 17:30:53 +0200

    Finished

--- app/src/main/java/com/example/madlevel1example/MainActivity.kt
+++ app/src/main/java/com/example/madlevel1example/MainActivity.kt
@@ -2,17 +2,40 @@ package com.example.madlevel1example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.madlevel1example.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        // Set a listener for the button so we can call checkAnswer() when the button is clicked
+        view.btnConfirm.setOnClickListener {
+            checkAnswer()
+        }
     }
 
     private fun checkAnswer(){
         val answer = etAnswer.text.toString()
+
+        // Check if the user answered the correct animal or not
+        if(answer == getString(R.string.giraffe)){
+            Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_LONG).show()
+        }
     }
+
+
 }
 
 
